aec41db07b1a978d2d84b26d45ff280905642d73
thekey2323 <122228404+thekey2323@users.noreply.github.com>
2026-01-14 09:29:28 +0100

    Update Dashboard.tsx

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, useMemo } from 'react';
 import { useApp } from '../context/AppContext';
 import { OrderStatus, AdPurpose, Product, Material } from '../types';
