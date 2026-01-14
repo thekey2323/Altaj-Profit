74608b8f6a0e4577866116ea5390212a3c04a53d
thekey2323 <122228404+thekey2323@users.noreply.github.com>
2026-01-14 09:29:57 +0100

    Update AppContext.tsx

--- context/AppContext.tsx
+++ context/AppContext.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
 import { Material, Product, Order, AdSpend, OrderStatus, AdPurpose } from '../types';
 
